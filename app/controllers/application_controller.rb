2464eb24c19b7987a5e2f5822aaa4f992183d070
hide <hide.taka.070401@gmail.com>
2019-04-04 11:47:04 +0900

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  # include Jpmobile::ViewSelector
+  protect_from_forgery with: :exception
+
+  before_action :set_view_path
+
+  private
+  def set_view_path
+    path =  request.smart_phone? ? 'smartphone' : 'pc'
+    prepend_view_path(Rails.root + 'app/views' + path)
+  end
 end
